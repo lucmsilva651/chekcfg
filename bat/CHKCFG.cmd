b706ec202e409749a909445dca49a1f4d1f17ed6
Lucas Gabriel <lucmsilva651@gmail.com>
2023-12-21 23:18:05 -0300

    Updated code

--- bat/CHKCFG.cmd
+++ bat/CHKCFG.cmd
@@ -1,17 +1,10 @@
 @echo off
+chcp 65001 >nul
 
-title CHEKCFG - Simple specifications viewer in CMD
-color b
-
-echo.
-echo 	CHEKCFG - Simple specifications viewer in CMD
+title CHKCFG ^<User: %USERNAME%^>
+echo 	CHKCFG ^<User: %USERNAME%^>
 echo 	Made by Lucas Gabriel (lucmsilva)
-echo.
-echo 	Follow my YouTube channel:
-echo 	https://www.youtube.com/@lucmsilvagg
-echo.
 echo 	------------------------------------------
-echo.
 echo 	Main computer info:
 echo.
 echo 	OneDrive folder: %OneDrive%
@@ -29,36 +22,13 @@ echo 	Public User Directory: %PUBLIC%
 echo 	Username: %USERNAME%
 echo 	Logon Server: %LOGONSERVER%
 echo 	LS + Username: %USERNAME%%LOGONSERVER%
-echo.
 echo 	------------------------------------------
-echo 	Click Enter to see processor info...
-pause >nul
-cls
-echo.
 echo 	Processor information:
 echo.
 echo 	Number of processors: %NUMBER_OF_PROCESSORS%
 echo 	Architeture: %PROCESSOR_ARCHITECTURE%
 echo 	Identifier: %PROCESSOR_IDENTIFIER%
 echo 	Revision: %PROCESSOR_REVISION%
-echo.
 echo 	------------------------------------------
 echo.
-echo 	Click Enter to see netstat info...
-pause >nul
-cls
-netstat -an
-echo.
-echo 	------------------------------------------
-echo.
-echo 	Click Enter to see more detailed general info...
-pause >nul
-cls
-systeminfo
-echo.
-echo 	------------------------------------------
-echo.
-echo 	Click Enter to exit...
-pause >nul
-cls
 pause >nul
