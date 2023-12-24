0af8f015425e4c3991b85749abfa368b638e19b3
Lucas Gabriel <lucmsilva651@gmail.com>
2023-12-23 22:10:52 -0300

    Update CHKCFG.bat

--- bat/CHKCFG.bat
+++ bat/CHKCFG.bat
@@ -5,12 +5,16 @@ set serialnumber=
 set manufacturer=
 set osname=
 set model=
-FOR /F "tokens=2 delims='='" %%A in ('wmic Bios Get SerialNumber /value') do SET serialnumber=%%A
-FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Manufacturer /value') do SET manufacturer=%%A
-FOR /F "tokens=2 delims='='" %%A in ('wmic os get Name /value') do SET osname=%%A
-FOR /F "tokens=1 delims='|'" %%A in ("%osname%") do SET osname=%%A
-FOR /F "tokens=2 delims='='" %%A in ('wmic ComputerSystem Get Model /value') do SET model=%%A
-
+FOR /F "tokens=2 delims='='" %%A in ('wmic BASEBOARD GET SERIALNUMBER /value') do SET serialnumber=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic BASEBOARD GET MANUFACTURER /value') do SET manufacturer=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic OS GET NAME /value') do SET osname=%%A
+FOR /F "tokens=1 delims='|'" %%A in ("%OSNAME%") do SET osname=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic COMPUTERSYSTEM GET MODEL /value') do SET model=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic BIOS GET SMBIOSBIOSVersion /value') do SET biosver=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic COMPUTERSYSTEM GET SYSTEMTYPE /value') do SET systype=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic BASEBOARD GET PRODUCT /value') do SET mbproduct=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic BASEBOARD GET VERSION /value') do SET mbvariant=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic OS GET WINDOWSDIRECTORY /value') do SET windowsdir=%%A
 :main
 title CHKCFG [User: %USERNAME%]
 echo.
@@ -19,26 +23,20 @@ echo 	2023 Lucas Gabriel (lucmsilva)
 echo 	-------------------------------------------------
 echo 	Main computer info: [Updates every ~1 sec.]
 echo.
-echo 	Date + Time: %DATE% ^|^ %TIME%
+echo 	Date + Time: %DATE% ▪ %TIME%
+echo 	Username: %USERNAME% ▪ User Path: %USERPROFILE%
 echo 	Computer Name: %COMPUTERNAME%
-echo 	Username: %USERNAME%
-echo 	User Path: %USERPROFILE%
-echo 	App Data: %APPDATA%
-echo 	Local App Data: %LOCALAPPDATA%
-echo 	Temporary folder: %TEMP%
-echo 	Public User folder: %PUBLIC%
-echo 	Manufacturer: %MANUFACTURER%
-echo 	Model: %MODEL%
-echo 	Serial Number: %SERIALNUMBER%
+echo 	Manufacturer: %MANUFACTURER% ▪ Model: %MODEL%
+echo 	Serial Number: %SERIALNUMBER% ▪ BIOS Version: %BIOSVER%
+echo 	Motherboard Model: %MBPRODUCT% ▪ Variant: %MBVARIANT%
+echo 	System Drive: %SystemDrive%\ ▪ System Root: %WINDOWSDIR%
+echo 	Program Files: %ProgramFiles% ▪ %ProgramFiles(x86)%
 echo 	Operating System: %OSNAME%
-echo 	System Drive: %SystemDrive%\
-echo 	System Root: %SystemRoot%
-echo 	Program Files: %ProgramFiles% ^|^ %ProgramFiles(x86)%
 echo 	-------------------------------------------------
 echo 	Processor information:
 echo.
 echo 	Number of processors (cores): %NUMBER_OF_PROCESSORS%
-echo 	Architeture: %PROCESSOR_ARCHITECTURE%
+echo 	System Type: %SYSTYPE%
 echo 	Identifier: %PROCESSOR_IDENTIFIER%
 echo 	Revision: %PROCESSOR_REVISION%
 echo 	-------------------------------------------------
