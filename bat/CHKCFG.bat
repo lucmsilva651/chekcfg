e9cc831d6273685e2cc2373760d58aa59ec9638f
Lucas Gabriel <lucmsilva651@gmail.com>
2023-12-29 14:00:20 -0300

    Update CHKCFG.bat

--- bat/CHKCFG.bat
+++ bat/CHKCFG.bat
@@ -1,11 +1,28 @@
 @echo off
 chcp 65001 >nul
 
+rem startup
+title CHKCFG [User: %USERNAME%]
+echo 	CHKCFG [User: %USERNAME%]
+echo 	2023 Lucas Gabriel (lucmsilva)
+echo 	-------------------------------------------------
+echo 	This project is in development. Incompatibilities with wmic
+echo 	can occur. If the program crashes past this screen, your
+echo 	Windows system isn't compatible with some method that I use
+echo 	to collect system data. Sorry for the inconvenience.
+echo 	But, if it works, give me a feedback sending a message on
+echo 	Telegram ^> https://t.me/lucmsilva651.
+echo 	-------------------------------------------------
+echo 	Wait 10 seconds or press any key to continue.
+timeout /t 10 >nul
+
+rem importing data from wmic
 FOR /F "tokens=2 delims='='" %%A in ('wmic BASEBOARD GET SERIALNUMBER /value') do SET serialnumber=%%A
 FOR /F "tokens=2 delims='='" %%A in ('wmic BASEBOARD GET MANUFACTURER /value') do SET manufacturer=%%A
 FOR /F "tokens=2 delims='='" %%A in ('wmic OS GET NAME /value') do SET osname=%%A
 FOR /F "tokens=1 delims='|'" %%A in ("%OSNAME%") do SET osname=%%A
 FOR /F "tokens=2 delims='='" %%A in ('wmic COMPUTERSYSTEM GET MODEL /value') do SET model=%%A
+FOR /F "tokens=2 delims='='" %%A in ('wmic CSPRODUCT GET VERSION /value') do SET secmodel=%%A
 FOR /F "tokens=2 delims='='" %%A in ('wmic BIOS GET SMBIOSBIOSVersion /value') do SET biosver=%%A
 FOR /F "tokens=2 delims='='" %%A in ('wmic COMPUTERSYSTEM GET SYSTEMTYPE /value') do SET systype=%%A
 FOR /F "tokens=2 delims='='" %%A in ('wmic BASEBOARD GET PRODUCT /value') do SET mbproduct=%%A
@@ -18,8 +35,9 @@ FOR /F "tokens=2 delims='='" %%A in ('wmic CPU GET NUMBEROFLOGICALPROCESSORS /va
 FOR /F "tokens=2 delims='='" %%A in ('wmic CPU GET VIRTUALIZATIONFIRMWAREENABLED /value') do SET virtenabled=%%A
 FOR /F "tokens=2 delims='='" %%A in ('wmic CPU GET CURRENTCLOCKSPEED /value') do SET cpuclock=%%A
 
+rem main update code
 :main
-title CHKCFG [User: %USERNAME%]
+cls
 echo.
 echo 	CHKCFG [User: %USERNAME%]
 echo 	2023 Lucas Gabriel (lucmsilva)
@@ -30,6 +48,7 @@ echo 	Date + Time: %DATE% ▪ %TIME%
 echo 	Username: %USERNAME% ▪ User Path: %USERPROFILE%
 echo 	Computer Name: %COMPUTERNAME%
 echo 	Manufacturer: %MANUFACTURER% ▪ Model: %MODEL%
+echo 	Model 2nd: %secmodel%
 echo 	Serial Number: %SERIALNUMBER% ▪ BIOS Version: %BIOSVER%
 echo 	Motherboard Model: %MBPRODUCT% ▪ Variant: %MBVARIANT%
 echo 	System Drive: %SystemDrive%\ ▪ System Root: %WINDOWSDIR%
@@ -45,6 +64,6 @@ echo 	CPU clock: %CPUCLOCK% MHz
 echo 	Identifier: %PROCESSOR_IDENTIFIER%
 echo 	Revision: %PROCESSOR_REVISION%
 echo 	-------------------------------------------------
+
 timeout /t 1 /nobreak >nul
-cls
 goto main
